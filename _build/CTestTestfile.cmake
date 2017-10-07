ea6f19d1ba6b3b72f45829f58ba8ad0d8cd86700
SimonRussia <git.streisand@gmail.com>
2017-10-07 15:54:39 +0300

    added tests

--- _build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anisim_ovcharov/documents/workspace/labs/future
-# Build directory: /Users/anisim_ovcharov/documents/workspace/labs/future/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/Users/anisim_ovcharov/documents/workspace/labs/future/_build/check" "-s" "-r" "compact" "--use-colour" "yes")
